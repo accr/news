f40b6772825830845b59621cd7a79aed2125f465
accr <574150795@qq.com>
2017-07-16 20:13:17 +0800

    mobile header
    import react-responsive

---
+++ src/js/components/mobile_header.js
@@ -0,0 +1,16 @@
+/**
+ * Created by AC on 2017/7/16.
+ */
+import React from 'react';
+export default class MobileHeader extends React.Component {
+    render() {
+        return(
+            <div id="mobileheader">
+                <header>
+                    <img src="./src/images/logo.png" alt="logo" />
+                    <span>ReactNews</span>
+                </header>
+            </div>
+        );
+    }
+}
---
+++ src/js/components/mobile_index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by AC on 2017/7/16.
+ */
+import React from 'react';
+import MobileHeader from './mobile_header';
+export default class MobileIndex extends React.Component {
+    render() {
+        return (
+            <div>
+                <MobileHeader/>
+            </div>
+        );
+    }
+}
