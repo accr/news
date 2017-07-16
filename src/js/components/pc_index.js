50a75eb04c51be47901f68849525e4a6100be5ce
accr <574150795@qq.com>
2017-07-16 19:44:58 +0800

    header
    logo nav

---
+++ src/js/components/pc_index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by AC on 2017/7/16.
+ */
+import React from 'react';
+import PCHeader from './pc_header';
+export default class PCIndex extends React.Component {
+    render() {
+        return(
+            <div>
+                <PCHeader/>
+            </div>
+        );
+    }
+}
--- src/js/root.js
+++ src/js/root.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {Button} from 'antd';
+import PCIndex from './components/pc_index';
 import 'antd/dist/antd.css';
 
 export default class Root extends React.Component {
 	render() {
 		return (
 			<div>
-
+				<PCIndex/>
 			</div>
 		);
 	};
