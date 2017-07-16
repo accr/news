2e3685b4b6240163924047a76a13283ae393e50a
accr <574150795@qq.com>
2017-07-16 20:37:28 +0800

    footer
    footer module

---
+++ src/js/components/mobile_footer.js
@@ -0,0 +1,22 @@
+/**
+ * Created by AC on 2017/7/16.
+ */
+import React from 'react';
+import { Row, Col } from 'antd';
+
+export default class MobileFooter extends React.Component {
+
+    render() {
+        return (
+            <footer>
+                <Row>
+                    <Col span={2} />
+                    <Col span={20}>
+                        &copy;&nbsp;2017 ReactNews. All Rights Reserved.
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+            </footer>
+        );
+    };
+}
