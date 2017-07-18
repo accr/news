e93ba16bb79320df511c96450cbe943ea0d087cf
accr <574150795@qq.com>
2017-07-18 21:35:03 +0800

    收藏功能开发
    PC端和移动端共用一个commonComment模块

--- src/js/components/common_comments.js
+++ src/js/components/common_comments.js
@@ -15,7 +15,8 @@ import {
     Button,
     Checkbox,
     Modal,
-    Card
+    Card,
+    notification
 } from 'antd';
 
 const FormItem = Form.Item;
@@ -48,7 +49,19 @@ class CommonComments extends React.Component {
         var formdata = this.props.form.getFieldsValue();
         fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey + "&commnet=" + formdata.remark, myFetchOptions).then(response => response.json()).then(json => {
             this.componentDidMount();
-        })
+        });
+    };
+
+    addUserCollection(){
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=uc&userid="+localStorage.userid+"&uniquekey="+this.props.uniquekey,myFetchOptions)
+            .then(response=>response.json())
+            .then(json=>{
+                //收藏成功以后进行一下全局的提醒
+                notification['success']({message: 'ReactNews提醒', description: '收藏此文章成功'});
+            });
     };
 
     render(){
@@ -72,6 +85,8 @@ class CommonComments extends React.Component {
                                 <Input type="textarea" placeholder="随便写" {...getFieldDecorator('remark',{initialValue:''})} />
                             </FormItem>
                             <Button type="primary" htmlType="submit">提交评论</Button>
+                            &nbsp;&nbsp;
+                            <Button type="primary" htmlType="button" onClick={this.addUserCollection.bind(this)}>收藏该文章</Button>
                         </Form>
                     </Col>
                 </Row>
